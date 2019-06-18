8ce471f0a911b3f22892b0c16bd2485760a711d4
silmin <silvestmint@gmail.com>
2019-06-18 23:59:31 +0900

    fix: renew venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/silmin/slot-api/venv"
+set -gx VIRTUAL_ENV "/home/silmin/dice-api/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
