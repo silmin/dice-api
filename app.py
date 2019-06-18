efaf32e77d17978d6d518ef11e109fd868e93f52
silmin <silvestmint@gmail.com>
2019-06-19 01:20:31 +0900

    add: roll()

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import random
 from flask import *
 
 app = Flask(__name__)
@@ -7,6 +8,44 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, I'm Dice API"
+
+@app.route("/roll", methods=['GET', 'POST'])
+def roll():
+    if request.method == 'GET': 
+        faces = request.args.get('faces')
+        cnt = request.args.get('cnt')
+        times = request.args.get('times')
+
+    elif request.method == 'POST': 
+        faces = request.json['faces']
+        cnt = request.json['cnt']
+        times = request.json['times']
+
+    faces = int(faces)
+    cnt = int(cnt)
+    times = int(times)
+    
+    result = []
+    for t in range(times):
+
+        r = []
+        for c in range(cnt):
+            top = random.randint(1, faces)
+            r.append(top)
+
+        result.append(r)
+
+    data = [
+        {"faces": faces},
+        {"cnt": cnt},
+        {"times": times},
+        {"result": result}
+    ]
+            
+    return jsonify({
+        "status": "OK",
+        "data": data
+        })
 
 if __name__ == "__main__":
     app.run()
